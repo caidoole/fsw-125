da9b242d4d501db61a97ff1b5ec68dd6d00c7629
caidoole <caitlin.dooley@bryanuniversity.edu>
2020-08-01 09:49:18 -0700

    wk one math

---
+++ wk1/math/app.js
@@ -0,0 +1,9 @@
+const {sum, subtraction, multiply, divide} = require('./math.js')
+
+console.log(sum(2, 15))
+
+console.log(subtraction(5, 3))
+
+console.log(multiply(2, 5))
+
+console.log(divide(4, 2))
